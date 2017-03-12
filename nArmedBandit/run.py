4ed9900ae4bc8aeaacde1df96e1140232b34a512
Nick Cellino <nacellin@buffalo.edu>
2017-03-11 21:18:06 -0500

    more data

--- nArmedBandit/run.py
+++ nArmedBandit/run.py
@@ -4,14 +4,10 @@ from Agent import EpsilonGreedyAgent, SoftmaxAgent
 agents = []
 labels = []
 num_arms = 10
-agents.append(EpsilonGreedyAgent(0.01, num_arms))
-labels.append("E-Greedy (e=0.01)")
-agents.append(EpsilonGreedyAgent(0.1, num_arms))
-labels.append("E-Greedy (e=0.1)")
-agents.append(SoftmaxAgent(.15, num_arms))
-labels.append("Softmax (t=0.15)")
-agents.append(SoftmaxAgent(.25, num_arms))
-labels.append("Softmax (t=0.25)")
+temps = [.25, .45, .65, .85]
+for temp in temps:
+    agents.append(SoftmaxAgent(temp, num_arms))
+    labels.append("Softmax (t=" + str(temp) + ")")
 
 tb = TestBed(agents, num_arms)
 tb.run()
